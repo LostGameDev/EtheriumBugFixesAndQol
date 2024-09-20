5711f6769177b9adb9cf01e640f4ba8701055cad
GameWorld <ehrenf@outlook.com>
2024-09-20 11:57:15 +1000

    Plugin.cs refactor

--- Plugin.cs
+++ Plugin.cs
@@ -7,9 +7,11 @@ namespace BugFixesAndQoL;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInProcess("Etherium.exe")]
+
 public class Plugin : BaseUnityPlugin
 {
-    internal static new ManualLogSource Logger;
+    public static new ManualLogSource Logger;
+    Fixes FixesInstance = new Fixes();
 
     private void Awake()
     {
@@ -19,26 +21,29 @@ public class Plugin : BaseUnityPlugin
 
         // Remove AVProWindowsMedia-x64.dll if it exists
         Logger.LogInfo("Checking for AVProWindowsMedia-x64.dll...");
-        CheckForAVProWindowsMediaX64Dll();
+        FixesInstance.CheckForAVProWindowsMediaX64Dll();
     }
+}
 
-    private bool CheckForAVProWindowsMediaX64Dll()
+public class Fixes
+{
+    private string GameRootDirectory = Directory.GetCurrentDirectory();
+    public bool CheckForAVProWindowsMediaX64Dll()
     {
-        string path = Directory.GetCurrentDirectory();
-        if (File.Exists($"{path}/Etherium_Data/Plugins/AVProWindowsMedia-x64.dll") != true)
+        if (File.Exists($"{GameRootDirectory}/Etherium_Data/Plugins/AVProWindowsMedia-x64.dll") != true)
         {
-            Logger.LogInfo("AVProWindowsMedia-x64.dll does not exist (This is a good thing! This means you wont experience the tutorial crash.)");
+            Plugin.Logger.LogInfo("AVProWindowsMedia-x64.dll does not exist (This is a good thing! This means you wont experience the tutorial crash.)");
             return false;
         }
         try
         {
-            File.Delete($"{path}/Etherium_Data/Plugins/AVProWindowsMedia-x64.dll");
-            Logger.LogInfo("Sucessfully removed AVProWindowsMedia-x64.dll (This fixes the tutorial crash.)");
+            File.Delete($"{GameRootDirectory}/Etherium_Data/Plugins/AVProWindowsMedia-x64.dll");
+            Plugin.Logger.LogInfo("Sucessfully removed AVProWindowsMedia-x64.dll (This fixes the tutorial crash.)");
             return true;
         }
         catch (Exception e)
         {
-            Logger.LogInfo($"Failed to remove AVProWindowsMedia-x64.dll due to {e} please quit the game and remove it manually!");
+            Plugin.Logger.LogInfo($"Failed to remove AVProWindowsMedia-x64.dll due to {e} please quit the game and remove it manually!");
             return false;
         }
     }
