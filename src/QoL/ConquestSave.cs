0f02b9d203bb8f7befd55e5783f25270865d85af
GameWorld <ehrenf@outlook.com>
2025-10-15 23:43:20 +1000

    Update EtheriumLib

--- src/QoL/ConquestSave.cs
+++ src/QoL/ConquestSave.cs
@@ -1,12 +1,18 @@
 ﻿using HarmonyLib;
-using Scaleform;
-using System.Reflection;
 using EtheriumLib.Debug;
+using UnityEngine;
 
 namespace BugFixesAndQoL
 {
 	[HarmonyPatch]
 	public static class ConquestSave
 	{
-	}
+        [HarmonyPatch(typeof(Game), "startTheGame")]
+        [HarmonyPrefix]
+        public static void Game_startTheGame_Prefix()
+        {
+            //InspectorDebugUtils.PrintHierarchy();
+            InspectorDebugUtils.PrintComponents(GameObject.Find("MultiplayerManager"));
+        }
+    }
 }
